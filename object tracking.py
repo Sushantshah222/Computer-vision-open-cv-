bfeb2889c7a0f1f7236e7991688fff8d30814d80
Sushant Shah <sushant.shah222@gmail.com>
2021-11-23 13:40:02 +0545

    Update object tracking.py

--- object tracking.py
+++ object tracking.py
@@ -1,37 +1,37 @@
 '''
 https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_colorspaces/py_colorspaces.html
 '''
-# import cv2
-# import numpy as np
+import cv2
+import numpy as np
 
-# cap = cv2.VideoCapture(1)
-#
-# while(True):
-#
-#     # Take each frame
-#     _, frame = cap.read()
-#
-#     # Convert BGR to HSV
-#     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-#
-#     # define range of blue color in HSV
-#     # lower_blue = np.array([110,50,50])
-#     lower_red = np.array([155,25,0])
-#     # upper_blue = np.array([130,255,255])
-#     upper_red = np.array([179,255,255])
-#
-#     # Threshold the HSV image to get only blue colors
-#     mask = cv2.inRange(hsv, lower_red, upper_red)
-#
-#     # Bitwise-AND mask and original image
-#     res = cv2.bitwise_and(frame,frame, mask= mask)
-#
-#     cv2.imshow('frame',frame)
-#     cv2.imshow('mask',mask)
-#     cv2.imshow('res',res)
-#     k = cv2.waitKey(5) & 0xFF
-#     if k == 27 :
-#         break
-#
-# cv2.destroyAllWindows()
+cap = cv2.VideoCapture(1)
+
+while(True):
+
+    # Take each frame
+    _, frame = cap.read()
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # define range of blue color in HSV
+    # lower_blue = np.array([110,50,50])
+    lower_red = np.array([155,25,0])
+    # upper_blue = np.array([130,255,255])
+    upper_red = np.array([179,255,255])
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27 :
+        break
+
+cv2.destroyAllWindows()
 
